bf9565883fb58ce737c64733c434c282c5f167fb
Mr-Yuxuan <945798300@qq.com>
2016-06-02 13:34:07 +0800

    选择排序

--- SelectSort/SelectSort/SelectSort.cpp
+++ SelectSort/SelectSort/SelectSort.cpp
@@ -19,10 +19,46 @@ void SelectSort(int *a, size_t n)
 	}
 	return;
 }
+//”≈ªØ∞Ê±æ
+void SelectSortII(int *a, size_t n)
+{
+	assert(a);
+	int start = 0;
+	int end = n - 1;
+	while (start < end)
+	{
+		int max = end;
+		int min = start;
+		for (size_t i = start; i <= end; i++)
+		{
+			if (a[i] <= a[min])
+				min = i;
+			if (a[i]>=a[max])
+				max = i;
+		}
+		if (max == start)
+		{
+			max = min;
+		}
+		swap(a[start], a[min]);
+		if (min == end)
+		{
+			min = max;
+		}
+		swap(a[end], a[max]);
+		
+		
+		
+		start++;
+		end--;
+	}
+	return;
+}
 int main()
 {
-	int a[] = { 1, 1, 6, 3, 0, 2, 4, 9, 7, 7 };
+	int a[] = {  4, 6, 3, 8, 2, 4,  7, 7,1,0 };
 	SelectSort(a, 10);
+	SelectSortII(a, 10);
 	system("pause");
     return 0;
 }
